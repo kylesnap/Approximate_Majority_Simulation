0264cfea2718eb3546390d558847f3bfdad2d85b
Kyle Dewsnap <snappydo@Kyles-MacBook-Pro.local>
2020-01-17 23:46:33 -0500

    Began working on approx majority interaction in network.py.

--- agents.py
+++ agents.py
@@ -3,20 +3,20 @@ from typing import List
 class Agent:
 
     def __init__(self, id, state) -> None:
-        self.id : int = id
-        self.state : str = state
-        self.__learnt : int = -1 #Set to -1 if haven't learnt yet.
+        self._ID : int = id
+        self._state : str = state
 
-    def __str__(self):
-        return str("ID: %5d, State: %c" % (self.id, self.state))
+    def __str__(self) -> str:
+        """Pretty print."""
+        return str("ID: %5d, State: %c" % (self._ID, self._state))
 
-    
+    def __eq__(self, other) -> bool:
+        """Returns true when the two agents are the same."""
+        if (self._ID == other._ID): return True
+        return False
 
-    
-    
+    def get_state(self) -> str:
+        return self._state
 
-    
-
-    
-
-    
+    def set_state(self, value: str) -> None:
+        self._state = value
--- network.py
+++ network.py
@@ -1,20 +1,28 @@
 import agents
+import random
+import warnings
+from typing import Dict, Type
+
+ACCEPTED_STATES = ['x', 'y', 'u']
 
 class Network:
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Constructs empty dictionary representing network."""
-        self.__units = dict()
-        self.length = 0
+        self.__units : Dict() = dict()
+        self.length : int = 0
 
-    def add_agents(self, n: int, state: str):
+    def add_agents(self, n: int, state: str) -> None:
         """Adds 'n' number of 'state' agents."""
+        if state not in ACCEPTED_STATES:
+            warnings.warn("Agents of this state are not supported. No agents were added.")
+            return
         for i in range(self.length, self.length + n):
             self.length += 1
             self.__units[self.length] = agents.Agent(self.length, state)
         print("%d number of %s-agents added to network." % (n, state))
 
-    def count_beliefs(self):
+    def count_beliefs(self) -> dict():
         """Returns a dictionary of agent type counts."""
         x = y = u = 0
         for agent in self.__units.values():
@@ -29,14 +37,39 @@ class Network:
                 raise ValueError('Unsupported agent found in network.')
         return {'nx' : x, 'ny' : y, 'nu' : u}
 
-    def clear_agents(self):
+    def clear_agents(self) -> None:
         """Clears network."""
         self.__units.clear()
         self.length = 0
         print("All units removed from the network.")
 
-    def print_all(self):
+    def print_all(self) -> None:
         """Prints a list of all the IDs and Agents."""
-        print("There are %d agents in this network." % self.length)
+        print("There are %d agents in this network.\n" % self.length)
         for agent in self.__units.values():
             print(agent)
+
+    def aprox_maj(self, id_init, id_recip) -> agents.Agent:
+        """The approximate majority algorithm for an exchange between two agents."""
+        init : agents.Agent = self.__units.get(id_init)
+        recip : agents.Agent = self.__units.get(id_recip)
+
+        if init is None or recip is None:
+            warnings.warn('ID[s] provided were not found in network.')
+            return None
+
+        if (init is recip or
+        init.get_state() is recip.get_state() or
+        init.get_state() is 'u'):
+            print('nothing happens')
+        else:
+            pass
+
+        return recip
+        
+        
+        
+        
+        
+
+        
