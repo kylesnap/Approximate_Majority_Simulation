d4939d05b38ae5401cd45029d2f87ad5433d18a4
Kyle Dewsnap <snappydo@Kyles-MacBook-Pro.local>
2020-01-25 13:27:36 -0500

    Added random number generator that was f*cking impossible to figure out.

--- agents.py
+++ agents.py
@@ -1,4 +1,5 @@
 from typing import List
+from scipy.stats import truncnorm
 
 class Agent:
 
@@ -20,3 +21,16 @@ class Agent:
 
     def set_state(self, value: str) -> None:
         self._state = value
+
+class P_Generator:
+
+    def __init__(self) -> None:
+        mean = float(input("What is the mean of your p distribution? [0.0, 1.0]"))
+        sd = float(input("What is the standard deviation of your p distribution?"))
+        low = 0
+        upp = 1
+        self._gen = truncnorm(
+        (low - mean) / sd, (upp - mean) / sd, loc=mean, scale=sd)
+
+    def get_p(self):
+        return self._gen.rvs()
