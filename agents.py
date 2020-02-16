e93abbcd33c494e8cd2ec78e99383bda71ec5a8c
snappydo <kyle.dewsnap@gmail.com>
2020-02-16 15:01:36 -0500

    Small corrections to formatting.

--- agents.py
+++ agents.py
@@ -1,22 +1,25 @@
 from typing import List
 
+
 class Agent:
 
-    def __init__(self, id, state) -> None:
-        self._ID : int = id
-        self._state : str = state
+    def __init__(self, id: int, state: str) -> None:
+        self._ID = id
+        self.__state = state
 
     def __str__(self) -> str:
         """Pretty print."""
-        return str("ID: %5d, State: %c" % (self._ID, self._state))
+        return str("ID: %5d, State: %2s" % (self._ID, self.__state))
 
     def __eq__(self, other) -> bool:
         """Returns true when the two agents are the same."""
         if (self._ID == other._ID): return True
         return False
 
-    def get_state(self) -> str:
-        return self._state
+    @property
+    def state(self) -> str:
+        return self.__state
 
-    def set_state(self, value: str) -> None:
-        self._state = value
+    @state.setter
+    def state(self, state: str) -> None:
+        self.__state = state
