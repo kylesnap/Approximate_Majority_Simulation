696da16d1eee9a23885ea6876c121a5090724f87
snappydo <kyle.dewsnap@gmail.com>
2020-02-16 15:01:10 -0500

    Add an implementation of the BAM algorithm.

--- network.py
+++ network.py
@@ -3,16 +3,17 @@ import random
 import warnings
 from typing import Dict, Type
 
-ACCEPTED_STATES = ['x', 'y', 'u']
+ACCEPTED_STATES = ['x', 'y', 'xy', 's']
+
 
 class Network:
 
     def __init__(self) -> None:
         """Constructs empty dictionary representing network."""
-        self.__units : Dict() = dict()
-        self.length : int = 0
+        self.__units = {}
+        self.length = 0
 
-    def add_agents(self, n : int, state : str) -> None:
+    def add_agents(self, n: int, state: str) -> None:
         """Adds 'n' number of 'state' agents."""
         if state not in ACCEPTED_STATES:
             warnings.warn("Agents of this state are not supported. No agents were added.")
@@ -20,31 +21,27 @@ class Network:
         for i in range(self.length, self.length + n):
             self.length += 1
             self.__units[self.length] = agents.Agent(self.length, state)
-        del i
-        #For testing
-        #print("%d number of %s-agents added to network." % (n, state))
 
     def count_beliefs(self) -> dict():
         """Returns a dictionary of agent type counts."""
-        x = y = u = 0
+        x = y = xy = 0
         for agent in self.__units.values():
-            state = agent.get_state()
+            state = agent.get_state
             if state == 'x':
                 x = x + 1
             elif state == 'y':
                 y = y + 1
             else:
-                u = u + 1
-        counts = {'nx' : x, 'ny' : y, 'nu' : u}
-        del x, y, u
+                xy = xy + 1
+        counts = {'nx': x, 'ny': y, 'nxy': xy}
         return counts
 
     def clear_agents(self) -> None:
         """Clears network."""
         self.__units.clear()
         self.length = 0
-        #For testing
-        #print("All units removed from the network.")
+        # For testing
+        # print("All units removed from the network.")
 
     def print_all(self) -> None:
         """Prints a list of all the IDs and Agents."""
@@ -52,22 +49,40 @@ class Network:
         for agent in self.__units.values():
             print(agent)
 
-    def aprox_maj(self) -> agents.Agent:
-        """The approximate majority algorithm for an exchange between two agents."""
-        init = self.__units.get(random.choice(list(self.__units)))
-        recip = self.__units.get(random.choice(list(self.__units)))
-
-        if init is None or recip is None:
-            warnings.warn('ID[s] provided were not found in network.')
-            return None
-
-        if (init == recip or
-        init.get_state() == recip.get_state() or
-        init.get_state() == 'u'):
-            pass
-        elif (recip.get_state() == 'u'):
-            recip.set_state(init.get_state())
+    def bam(self, i: int = -1, r: int = -1) -> agents.Agent:
+        """Implements the BAM algorithm."""
+        if i == -1 or r == -1:
+            init = self.__units.get(random.choice(list(self.__units)))
+            recip = self.__units.get(random.choice(list(self.__units)))
         else:
-            recip.set_state('u')
+            init = self.__units.get(i)
+            recip = self.__units.get(r)
+        choice = random.choice(['x', 'y'])
+
+        if recip.state == 's':
+            pass  # Stubborn agents won't learn.
+        elif recip.state == init.state:
+            if recip.state == 'xy':   # If both agents are compound, they'll come to both share a state at random.
+                recip.state = init.state = choice
+        elif init.state == 's':
+            if recip.state == 'xy':
+                recip.state = 'y'
+            elif recip.state == 'x':
+                recip.state = 'xy'
+        elif init.state == 'xy':  # If the initiator is compound, it will choose to share a state at random.
+            if recip.state == choice:
+                init.state = choice
+            else:
+                recip.state = 'xy'
+        else:
+            if recip.state == 'xy':
+                recip.state = init.state
+            else:
+                recip.state = 'xy'
 
         return recip
+
+
+def aprox_maj(self) -> agents.Agent:
+    """The approximate majority algorithm for an exchange between two agents."""
+    pass
